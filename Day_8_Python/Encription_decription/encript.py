aa379e07d3feadaae1b1d055934f2846e534dd85
Vivek V <Vivek.V@volvo.com>
2026-03-29 10:53:43 +0100

    modification in encription and decription

--- Day_8_Python/Encription_decription/encript.py
+++ Day_8_Python/Encription_decription/encript.py
@@ -4,11 +4,6 @@ def comment():
     print("""Encript the message using Caesar cipher""")
     print("*" * 50)
 
-def comment_1():
-    print("*" * 50)
-    print("""Encripted the message using Caesar cipher""")
-    print("*" * 50)
-
 def encript(message, shift):
     comment()
     #Encription code starts from here
@@ -16,15 +11,12 @@ def encript(message, shift):
     for i in message:
         # print(i)
         if i in init_object.alphabet:
-            a = init_object.alphabet.index(i) + shift
-            a %= 52
+            a = (init_object.alphabet.index(i) + shift) % 52
             init_object.encripted_message += init_object.alphabet[a]
         elif i in init_object.symbols:
             print(i)
-            a = init_object.symbols.index(i) + shift
-            a %= 30
+            a = (init_object.symbols.index(i) + shift) % 30
             init_object.encripted_message += init_object.symbols[a]
-    comment_1()
     print("-" * 50)
     print("Encripted message is: " + init_object.encripted_message)
     print("-" * 50)
